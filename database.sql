0757dd32407cb1ac83498020626dbbe72925bac5
iqbal <iqbalramdhany2410@gmail.com>
2025-07-20 15:07:14 +0700

    update databases2

--- database.sql
+++ database.sql
@@ -14,11 +14,58 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping database structure for kehadiran_db
+CREATE DATABASE IF NOT EXISTS `kehadiran_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `kehadiran_db`;
 
--- Data exporting was unselected.
+-- Dumping structure for table kehadiran_db.attendance
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `status` enum('Hadir','Izin','Sakit','Alpa') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table kehadiran_db.attendance: ~4 rows (approximately)
+INSERT INTO `attendance` (`id`, `name`, `status`) VALUES
+	(1, 'Iqbal', 'Hadir'),
+	(2, 'dadad', 'Sakit'),
+	(3, 'wdaad', 'Hadir'),
+	(4, '\'', 'Hadir');
+
+-- Dumping structure for table kehadiran_db.employee
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `passport` varchar(50) DEFAULT NULL,
+  `seamanbook` varchar(50) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table kehadiran_db.employee: ~0 rows (approximately)
+
+-- Dumping structure for table kehadiran_db.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `is_enable` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Index 2` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table kehadiran_db.users: ~3 rows (approximately)
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `name`, `is_enable`) VALUES
+	(1, 'admin', '0192023a7bbd73250516f069df18b500', 'admin', 'iqbal', 1),
+	(9, 'iqbal', 'efccfd8c96fae03c4d7c412064092303', 'user', 'None', 0),
+	(11, 'nustial', 'a63da0e4bf8f366cc100e48e68fe8a23', 'guest', NULL, 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
